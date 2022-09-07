5f7e0f5bf7f59d7e2a017458d2bf510df8b84fe9
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-09-07 16:05:23 +0200

    Min and Max Validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-
+#validate if title. description is empty
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
