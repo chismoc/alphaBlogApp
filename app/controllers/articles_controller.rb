da20bfc16c5b63fdbc362985f05ca45f2cd7d2da
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-09-08 12:27:20 +0200

    add article list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+#get all articles using .each
+    def index
+        @articles = Article.all
+    end
 end
