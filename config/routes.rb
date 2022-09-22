4b17fa132fea6b4101c431f0ad00e67ef785cfab
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-09-22 15:12:44 +0200

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#home"
   get 'about', to: 'pages#about'
   get 'landing', to: 'pages#landing'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 
   #give only routes needed
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
   #expose all the routes, Rest-ful routes
   resources :articles
+  resources :users, except:[:new]
 end
