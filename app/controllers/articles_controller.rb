1823bd0754505fea07b7acc6551cdf1fc08167f5
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-09-27 19:14:53 +0200

    controller methods as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     def create
       #for testing:  render plain: params[:article]
       @article = Article.new(article_params)
-      @article.user = User.first
+      @article.user = current_user
     
       #check if save happened
       if
