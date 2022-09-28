ca3f8aee61747c3754a6acb6cfdca85e35cf6aff
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-09-28 11:24:59 +0200

    add delete user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :show, :edit, :update]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   # GET /users or /users.json
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
       end
     end
 
+    def destroy
+      @user.destroy
+      session[:user_id] = nil 
+      flash[:notice] = "Account and all associated articls successfully deleted"
+      redirect_to articles_path
+    end
+
      # PATCH/PUT /users/1 or /users/1.json
   def update
    
@@ -47,6 +54,7 @@ class UsersController < ApplicationController
      render 'edit'
     end
    end
+   
 
     private 
     def user_params
