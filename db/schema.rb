19496654a18ae606257730637230aa24c4bf93ad
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-10-05 16:51:19 +0200

    add many to many asscociation to article and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_101918) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_133051) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
